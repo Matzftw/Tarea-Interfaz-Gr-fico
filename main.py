5d8d2f8cb497a508fd1bcd588fee3634ba6847a6
Matzftw <m.quiros.6@estudiantec.cr>
2026-03-26 14:55:06 -0600

    Primeros imports
    Imports importantes

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 
 """Tarea 1 Max Andrés Quirós Hernández Intro a la progra"""
 
-import math as mt
-
+import tkinter as tk
+import math as mt   #import de prueba
+import pygame #Solo para audio
+from PIL import Image #Solo para imagenes
